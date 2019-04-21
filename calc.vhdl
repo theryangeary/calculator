603bb8a5077816677c1e642ac560f7854438ebd4
Ryan Geary <rtgnj42@gmail.com>
2019-04-21 13:28:43 -0400

    Add PC components

--- calc.vhdl
+++ calc.vhdl
@@ -12,15 +12,100 @@ port(
       op5: in std_logic;
       op6: in std_logic;
       op7: in std_logic;
-      clk: in std_logic;
+      clk: in std_logic
     );
 end calc;
 
 architecture behav of calc is
-  signal PCrd, PCwr, rd1, rd2, wd, ALUOut, ImmEx: std_logic_vector(7 downto 0);
+  signal PC, PCrd, PCwr, rd1, rd2, wd, ALUOut, ImmEx: std_logic_vector(7 downto 0);
   signal CmpInc, PCInc: std_logic_vector(1 downto 0);
-  signal Cmp, Imm, WrEn, ALUOp: std_logic;
+  signal rs1, rs2, ws, jump_amt: std_logic_vector(1 downto 0);
+  signal imm: std_logic_vector(3 downto 0);
+  signal Cmp, WdSel: std_logic_vector(0 downto 0);
+  signal WrEn, ALUOp: std_logic;
 begin
+
+  rs1 <= op3 & op2;
+  rs2 <= op1 & op0;
+  ws  <= op5 & op4;
+  imm <= op3 & op2 & op1 & op0;
+  jump_amt <= op4 & '1';
+
+  PCrd <= PC;
+  PC <= PCwd;
+
+  reg: entity work.regfile
+  port map(
+    rs1 => rs1,
+    rs2 => rs2,
+    ws => ws,
+    wd => wd,
+    WE => WrEn,
+    clk => clk
+  );
+
+  wd_choice: entity work.mux
+  generic map(
+    N => 1
+  )
+  port map(
+    A => ALUOut,
+    B => ImmEx,
+    C => ALUOut,
+    D => ImmEx,
+    sel => WdSel,
+    O => wd
+  );
+
+  alu: entity work.add_sub
+  generic map(
+    N => 8
+  )
+  port map(
+    A => rd1,
+    B => rd2,
+    sel => ALUOp,
+    clock => clk,
+    O => ALUOut
+  );
+
+  cmp_adder: entity work.add_sub
+  generic map(
+    N => 2
+  )
+  port map(
+    A => jump_amt,
+    B => "01",
+    sel => '0',
+    clock => clk,
+    O => CmpInc
+  );
+
+  pc_mux: entity work.mux
+  generic map(
+    N => 1,
+    INLENGTH => 2
+  )
+  port map(
+    A => CmpInc,
+    B => "01",
+    C => CmpInc,
+    D => "01",
+    sel => Cmp,
+    O => PCInc
+  );
+
+  pc_adder: entity work.add_sub
+  generic map(
+    N => 8
+  )
+  port map(
+    A => PCrd,
+    B => PCInc,
+    sel => '0',
+    clock => clk,
+    O => PCwr
+  );
 
 end behav;
 
